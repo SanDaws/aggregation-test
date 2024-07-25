477ee4cfb9721e220375ddc063c82bbed036f313
sanDaws <castrogiraldosantiago@gmail.com>
2024-07-25 08:40:22 -0500

    fix:  adding admin class attributes and empty methods

---
+++ Models/AdministradorApp.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aggregation_test.Models;
+
+public static class AdministradorApp{
+    public static List<Estudiante> Estudiantes= new List<Estudiante>();
+    public static List<Profesor> Profesores= new List<Profesor>();
+
+    public static void AgregarEstudiante(Estudiante estudiante){}
+
+    public static void AgregarProfesor(Profesor profesor){}
+    public static void MostrarEstudiante(){}
+    public static void MostrarProfesores(){}
+
+
+
+
+}
