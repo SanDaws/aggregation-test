6e4247d079a766a9cc1496b127ef36776b356962
sanDaws <castrogiraldosantiago@gmail.com>
2024-07-25 09:47:24 -0500

    feat: basci solution of methods in static class

--- Models/AdministradorApp.cs
+++ Models/AdministradorApp.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,11 +10,27 @@ public static class AdministradorApp{
     public static List<Estudiante> Estudiantes= new List<Estudiante>();
     public static List<Profesor> Profesores= new List<Profesor>();
 
-    public static void AgregarEstudiante(Estudiante estudiante){}
+    public static void AgregarEstudiante(Estudiante estudiante){
+        Estudiantes.Add(estudiante);
+    }
 
-    public static void AgregarProfesor(Profesor profesor){}
-    public static void MostrarEstudiante(){}
-    public static void MostrarProfesores(){}
+    public static void AgregarProfesor(Profesor profesor){
+        Profesores.Add(profesor);
+    }
+    public static void MostrarEstudiantes(){
+        foreach (Estudiante estudiante in Estudiantes)
+        {
+            estudiante.MostrarDetalles();
+        }
+
+    }
+    //show all the teachers that are in the list
+    public static void MostrarProfesores(){
+        foreach (Profesor profesor in Profesores)
+        {
+            profesor.MostrarDetalles();
+        }
+    }
 
 
 
