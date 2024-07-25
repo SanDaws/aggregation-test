f009da85d41aa93ec881621d4b09090f3920b88e
sanDaws <castrogiraldosantiago@gmail.com>
2024-07-25 13:07:02 -0500

    fix: adding solution and adding more stuff

--- Models/Profesor.cs
+++ Models/Profesor.cs
@@ -8,13 +8,24 @@ namespace aggregation_test.Models;
 public class Profesor:Persona{
     public string Asignatura{get;set;}
     private double Salario{get;set;}
-    public DateOnly FechaContratacion{get;set;}
+    public DateTime FechaContratacion{get;set;}
     public List<string>Cursos;
 
-    public int CalcularSalario(){
-        return 0;
+    public Profesor(string Nombre,string Apellido,string TipoDocumento,string NumeroDocumento,string Email,string Telefono,string Asignatura,double Salario,int dia,int mes, int year):base(Nombre,Apellido,TipoDocumento,NumeroDocumento,Email,Telefono){
+        this.Asignatura=Asignatura;
+        this.Salario=Salario;
+        FechaContratacion= new DateTime(year,mes,dia);
+
+
     }
-    public void ObtenerSalario(){}
+
+    public int CalcularAntiguedad(){
+        int antiguedad= FechaContratacion.CompareTo(DateTime.Now);
+        return antiguedad;
+    }
+    public void ObtenerSalario(){
+        Console.WriteLine("Salario del profesor: {0}",Salario);
+    }
     public override void MostrarDetalles()
     {
         
