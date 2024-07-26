4737d09d7242964f0fe57d160b836e704b01a613
sanDaws <castrogiraldosantiago@gmail.com>
2024-07-26 10:39:05 -0500

    Feat: exception managmet class

---
+++ Models/exceptions.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aggregation_test.Models;
+
+public static class exceptions
+{
+    /*
+    Esta clase se la dedico a la humanidad:
+    si no fuera por ustedes no habria tenido que crearla
+    */
+    public static int safeInt(string arg)
+    {
+        int option = 0;
+        Console.WriteLine(arg);
+        try
+        {
+            option = Convert.ToInt32(AntiEMptyorNull());
+        }
+        catch (System.FormatException)
+        {
+            Console.WriteLine("Respuesta No valida");
+            safeInt("imposibe convertir, intente con un numero");
+
+        }
+        return option;
+
+    }
+    public static double safeDouble(string arg)
+    {
+        double option= 0;
+        Console.WriteLine(arg+": ");
+        try
+        {
+            option = Convert.ToDouble(AntiEMptyorNull());
+        }
+        catch (System.FormatException)
+        {
+            Console.WriteLine("Respuesta No valida");
+            safeInt("imposibe convertir, intente con un numero");
+
+        }
+        return option;
+
+    }
+
+    public static string AntiEMptyorNull(){
+            string texto;
+        do
+        {
+            texto=Console.ReadLine().Trim();
+            if (string.IsNullOrWhiteSpace(texto))
+            {
+                Uti.TextoRojo("Se prohibe texto vacio, ingrese datos ");
+            }
+        } while (string.IsNullOrWhiteSpace(texto));
+
+        return texto;
+    }
+}
